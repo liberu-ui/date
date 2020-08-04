9bf9dc98e74c9dcab0e0ac10ec213d1162e9a88b
reza <raftx24@gmail.com>
2020-08-04 20:18:57 +0430

    removes enso

--- src/ensoFormatDistance.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line import/no-unresolved
-import store from '@root/store';
-import formatDistance from './formatDistance';
-
-export default date => formatDistance(date, store.state.preferences.global.lang);
