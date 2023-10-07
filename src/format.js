0aa3ae0cf1f0f7bd892ac83ee7fddaf7b5caea66
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-07 20:36:31 +0000

    Replace JavaScript with TypeScript

--- src/format.js
+++
@@ -1,9 +0,0 @@
-import { format, parseISO } from 'date-fns/esm';
-import i18n from './i18n';
-import unicode2PHP from './unicode2php';
-
-export default (date, formatStr, locale = 'en') => format(
-    (typeof date === 'string') ? parseISO(date) : date,
-    unicode2PHP(formatStr),
-    { locale: i18n[locale] },
-);
--- src/formatDistance.js
+++
@@ -1,9 +0,0 @@
-import { formatDistance, parseISO } from 'date-fns/esm';
-// eslint-disable-next-line import/no-unresolved
-import i18n from './i18n';
-
-export default (date, locale = 'en') => formatDistance(
-    (typeof date === 'string') ? parseISO(date) : date,
-    new Date(),
-    { locale: i18n[locale] },
-);
